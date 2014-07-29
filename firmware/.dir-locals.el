e22b52c3e75886c0555b5f29dfe75461939df605
Richard Eoin Meadows <richardeoin@gmail.com>
2014-07-29 21:07:12 +0100

    Updated gdb command for emacs 24

--- firmware/.dir-locals.el
+++ firmware/.dir-locals.el
@@ -1,6 +1,6 @@
 ;; Emacs settings for this project
 ;; Copyright (C) 2013  Richard Meadows
-;; 
+;;
 ;; Permission is hereby granted, free of charge, to any person obtaining
 ;; a copy of this software and associated documentation files (the
 ;; "Software"), to deal in the Software without restriction, including
@@ -8,10 +8,10 @@
 ;; distribute, sublicense, and/or sell copies of the Software, and to
 ;; permit persons to whom the Software is furnished to do so, subject to
 ;; the following conditions:
-;; 
+;;
 ;; The above copyright notice and this permission notice shall be
 ;; included in all copies or substantial portions of the Software.
-;; 
+;;
 ;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 ;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 ;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -27,7 +27,6 @@
 ;; Keep this directory as the default directory when navigating subdirectoies
 
 ((nil
-  (gud-gdb-command-name . "arm-none-eabi-gdb --annotate=3 --command=gdbscript")
+  (gud-gdb-command-name . "arm-none-eabi-gdb -i=mi")
   (eval setq default-directory
 	(locate-dominating-file buffer-file-name ".dir-locals.el"))))
-
